e0d1d0682c0993f178e7a9642c3dbb76fd14b165
shahbaz <kingshahbaz0313@gmail.com>
2023-06-03 14:24:32 +0500

    Added background Image

--- src/App.js
+++ src/App.js
@@ -6,20 +6,6 @@ import InputFeild from "./components/InputComponent/InputFeild";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  // const [name,setName] = useState("");
-
-  // useEffect(()=>{
-
-  //   let data = localStorage.getItem("username")
-  //   console.log("--->",data)
-  //   setName(data)
-  // },[])
-
-  //   function handleClick(){
-  //     let inputValue = document.getElementById("input").value
-  //     setName(inputValue);
-  //     localStorage.setItem("username",inputValue)
-  //   }
   return (
     <div className="App font-custom">
       <Header />
